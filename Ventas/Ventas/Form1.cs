001475ddc5267732459200ceca84b42cc47a3708
Josue <Josue@JOSUE-PC>
2021-02-03 13:00:50 -0600

    agregando frase motivadora

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo");
+            MessageBox.Show("Si puedes soñarlo puedes logralo");
         }
     }
 }
